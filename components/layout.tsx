4e4e0ca9efbd35f018f96c38b5c58f5e125d8aa4
redips <redips@prismweaver.com>
2022-06-23 17:17:22 +0900

    [ksj] tailwindcss 적용 .next 폴더 reset 필요하다.

--- components/layout.tsx
+++ components/layout.tsx
@@ -14,14 +14,16 @@ export default function Layout({
         <Head>
             <title>{title}</title>
         </Head>
-        <div>
-            <img src="" alt=""/>
-            <div className={""}>
-                <h2>홈</h2>
-                <h2>기업 리뷰</h2>
-                <h2>채용 공고</h2>
-                <button>글쓰기</button>
-                <button>로그인</button>
+        <div className={"flex justify-between p-2 border-b-2"}>
+            <div className={"flex space-x-5 items-end p-2"}>
+                <div className={"text-4xl font-bold"}>Kimsajang</div>
+                <div className={"ml-6  text-2xl font-bold"}>홈</div>
+                <div className={"text-2xl font-bold"}>메뉴1</div>
+                <div className={"text-2xl font-bold"}>메뉴2</div>
+            </div>
+            <div className={"flex p-2"}>
+                <button className={"text-white bg-zinc-600 p-3 w-24"}>글쓰기</button>
+                <button className={"ml-2 border border-zinc-600 w-24 p-3"}>로그인</button>
             </div>
         </div>
         <div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,10 @@ import Layout from "@components/layout";
 
 const Home: NextPage = () => {
     return (
-        <Layout title={"kimsajang"}>
-            <div>
-                <h1 className={"text-3xl"}></h1>
-                Main
-            </div>
+        <Layout title="kimsajang">
+
+            <h1 className="text-blue-500"> Hello </h1>
+
         </Layout>
     )
 }
