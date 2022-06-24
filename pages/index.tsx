5dfff10692ddf3df4cbd6d5f78c58162b17689fe
redips <redips@prismweaver.com>
2022-06-24 23:28:10 +0900

    [ksj] 랭킹 카드 영역

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type {NextPage} from 'next'
 import Layout from "@components/layout";
 import MainCard from "@components/mainCard";
-import {faCoins, faComments, faCrown, faHeart, faHouse} from "@fortawesome/free-solid-svg-icons";
+import {faCoins, faComments, faCrown, faEye, faHeart, faHouse} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 const Home: NextPage = () => {
     return (
@@ -16,21 +18,62 @@ const Home: NextPage = () => {
                 </div>
             </div>
             <div className={"hidden xl:block"}>
-                <div className={"grid grid-cols-3 gap-8"}>
-                    <div className={"col-span-2 row-span-2"}>
-                        <MainCard topic={"토픽 베스트"} icon={faCrown}></MainCard>
+                <div className={"flex"}>
+                    <div className={"basis-2/3 grid grid-cols-2 gap-8"}>
+                        <div className={"col-span-2 row-span-2"}>
+                            {/*<MainCard topic={"토픽 베스트"} icon={faCrown}></MainCard>*/}
+                            <div className={"mx-2 p-2 border-b-8"}>
+                                <div className={" border-b-2 pb-2 py-3 font-bold text-xl"}>
+                                    <span className={"ml-3"}>토픽 베스트</span>
+                                </div>
+                                <div>
+                                    {
+                                        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(value => {
+                                            return <div key={value} className={"flex justify-between border-b-2 py-3"}>
+                                                <div className={"font-bold"}>토픽 베스트 👍👍</div>
+                                                <div className={"text-gray-400 flex space-x-2 items-center"}>
+                                                    <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+                                                    <span>597</span>
+                                                </div>
+                                            </div>
+                                        })
+                                    }
+                                </div>
+                                <Link href={`/topic/}`}>
+                                    <div className={"flex justify-center items-center h-10 text-gray-400" +
+                                        " cursor-pointer"}>더보기
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                        <div>
+                            <MainCard topic={"블라블라"} icon={faComments}></MainCard>
+                        </div>
+                        <MainCard topic={"썸 연예"} icon={faHeart}></MainCard>
+                        <MainCard topic={"주식 투자"} icon={faCoins}></MainCard>
+                        <MainCard topic={"부동산"} icon={faHouse}></MainCard>
                     </div>
-                    {/*<div className={"col-span-1 row-span-full"}>dfdf</div>*/}
-                    <div>
-                        <MainCard topic={"블라블라"} icon={faComments}></MainCard>
+                    <div className={"basis-1/3"}>
+                        <div className={"bg-gray-100 p-6 m-6 rounded-xl"}>
+                            <div className={"font-bold p-2 text-xl"}>실시간 인기 가게</div>
+                            {
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(value => {
+                                    return <div key={value} className={"flex justify-between p-2"}>
+                                        <div className={"flex space-x-2 "}>
+                                            <div className={"flex justify-end w-2"}>{value}</div>
+                                            <div>삼겹살</div>
+                                        </div>
+                                        <div>순위 조정</div>
+                                    </div>
+                                })
+                            }
+                        </div>
                     </div>
-                    <MainCard topic={"썸 연예"} icon={faHeart}></MainCard>
-                    <MainCard topic={"주식 투자"} icon={faCoins}></MainCard>
-                    <MainCard topic={"부동산"} icon={faHouse}></MainCard>
                 </div>
+
             </div>
         </Layout>
-    )
+    );
 }
 
 export default Home
