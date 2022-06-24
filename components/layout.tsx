e14f8331df94a7d0d81f0e0b6a7a6415621852a0
redips <redips@prismweaver.com>
2022-06-24 09:52:03 +0900

    [ksj] footer, 디자인의 경우 tailwindCSS로 직접 해보고, tailwindUI로도 경험해 볼 생각

--- components/layout.tsx
+++ components/layout.tsx
@@ -17,20 +17,35 @@ export default function Layout({
         <div className={"flex justify-between p-2 border-b-2"}>
             <div className={"flex space-x-5 items-end p-2"}>
                 <div className={"text-4xl font-bold"}>Kimsajang</div>
-                <div className={"ml-6  text-2xl font-bold"}>홈</div>
+                <div className={"text-2xl font-bold"}>홈</div>
                 <div className={"text-2xl font-bold"}>메뉴1</div>
                 <div className={"text-2xl font-bold"}>메뉴2</div>
             </div>
             <div className={"flex p-2"}>
-                <button className={"text-white bg-zinc-600 p-3 w-24"}>글쓰기</button>
-                <button className={"ml-2 border border-zinc-600 w-24 p-3"}>로그인</button>
+                <button className={"mx-2 text-white bg-zinc-500 p-3 w-24"}>글쓰기</button>
+                <button className={"mx-2 border border-zinc-500 w-24 p-3"}>로그인</button>
             </div>
         </div>
         <div>
             {children}
         </div>
-        <div>
-            Footer
+        <div className={"flex justify-between items-center  text-lg"}>
+            <div className={"flex-col justify-center items-center"}>
+                <div className={"flex"}>
+                    <a className={"mx-2"}>서비스 소개</a> |
+                    <a className={"mx-2"}>이용약관</a> |
+                    <a className={"mx-2"}>디렉토리</a> |
+                    <a className={"mx-2"}>개인정보 처리방침</a> |
+                    <a className={"mx-2"}>신고 가이드</a>
+                </div>
+                <div className={"flex mt-3"}>
+                    @2022 Prismweaver Inc.
+                </div>
+            </div>
+            <div className={"flex"}>
+                <button className={"mx-2 px-3 py-2 bg-zinc-200 rounded-full"}>APP STORE</button>
+                <button className={"mx-2 px-3 py-2 bg-zinc-200 rounded-full"}>GOOGLE PLAY</button>
+            </div>
         </div>
     </div>
 }
