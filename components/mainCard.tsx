10a575d41406461f5bd40f25343f09e9495576f4
redips <redips@prismweaver.com>
2022-06-24 16:24:03 +0900

    [ksj] main mobile

---
+++ components/mainCard.tsx
@@ -0,0 +1,40 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEye} from "@fortawesome/free-solid-svg-icons";
+import {IconDefinition} from "@fortawesome/fontawesome-common-types";
+import Link from "next/link";
+
+interface MainCardProps {
+    topic: string;
+    icon: IconDefinition;
+}
+
+export default function MainCard({
+                                     topic,
+                                     icon
+                                 }: MainCardProps) {
+
+    return (
+        <div className={"p-2 border-b-8"}>
+            <div className={"ml-2 border-b-2 pb-2 py-3 font-bold text-xl"}>
+                <FontAwesomeIcon icon={icon}></FontAwesomeIcon>
+                <span className={"ml-3"}>{topic}</span>
+            </div>
+            <div>
+                {
+                    [1, 2, 3, 4, 5].map(value => {
+                        return <div key={value} className={"flex justify-between border-b-2 py-3"}>
+                            <div className={"font-bold"}>블라블라 글 제목</div>
+                            <div className={"text-gray-400 flex space-x-2 items-center"}>
+                                <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+                                <span>597</span>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
+            <Link href={`/topic/${topic.replaceAll(' ', '-')}`}>
+                <div className={"flex justify-center items-center h-10 text-gray-400"}>더보기</div>
+            </Link>
+        </div>
+    )
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import type {NextPage} from 'next'
 import Layout from "@components/layout";
+import MainCard from "@components/mainCard";
+import {faCoins, faComments, faCrown, faHeart, faHouse} from "@fortawesome/free-solid-svg-icons";
 
 const Home: NextPage = () => {
     return (
         <Layout title="kimsajang">
-            <h1 className="text-blue-500"> Hello </h1>
+            <div className={"grid xl:grid-cols-2 gap-8"}>
+                <MainCard topic={"토픽 베스트"} icon={faCrown}></MainCard>
+                <MainCard topic={"블라블라"} icon={faComments}></MainCard>
+                <MainCard topic={"썸 연예"} icon={faHeart}></MainCard>
+                <MainCard topic={"주식 투자"}  icon={faCoins}></MainCard>
+                <MainCard topic={"부동산"} icon={faHouse}></MainCard>
+            </div>
         </Layout>
     )
 }
