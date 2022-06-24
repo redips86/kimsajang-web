db5e7161ce0961998d33ff3fd61a1e7ad23af37b
redips <redips@prismweaver.com>
2022-06-24 14:52:45 +0900

    [ksj] header, footer 반응형 적용, grid 자체로 제어하고 싶은데 방법이 마땅치 않아서 hidden으로 control했다.

--- components/layout.tsx
+++ components/layout.tsx
@@ -18,7 +18,7 @@ export default function Layout({
         <Head>
             <title>{title}</title>
         </Head>
-        <div className={"flex justify-between pb-2 border-b-2"}>
+        <div className={"lg:hidden flex justify-between pb-2 border-b-2"}>
             <div className={"flex space-x-5 items-end p-2"}>
                 <div className={"text-4xl font-bold"}>Kimsajang</div>
                 <div className={"text-2xl font-bold"}>홈</div>
@@ -35,24 +35,42 @@ export default function Layout({
                 <button className={"mx-2 border border-zinc-500 p-3 w-24"}>로그인</button>
             </div>
         </div>
+        <div className={"hidden lg:block"}>
+            <div className={"flex justify-between pb-2 border-b-2"}>
+                <div className={"flex space-x-5 items-end p-2"}>
+                    <div className={"text-4xl font-bold"}>Kimsajang</div>
+                </div>
+                <div className={"flex items-center p-2"}>
+                    <Link href={"/search"}>
+                        <button className={"w-6 h-6"}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} className={"w-full h-full"}></FontAwesomeIcon>
+                        </button>
+                    </Link>
+                    <button className={"mx-2 text-white bg-zinc-500 p-3 w-24"}>글쓰기</button>
+                    <button className={"mx-2 border border-zinc-500 p-3 w-24"}>로그인</button>
+                </div>
+            </div>
+            <div className={"flex pb-2 border-b-8"}>
+                <div className={"flex p-2"}>
+                    <div className={"text-2xl font-bold"}>홈</div>
+                    <div className={"text-2xl font-bold ml-2"}>메뉴1</div>
+                    <div className={"text-2xl font-bold ml-2"}>메뉴2</div>
+                </div>
+            </div>
+        </div>
         <div>
             {children}
         </div>
-        <div className={"flex justify-between items-center  text-lg mt-2 border-t-2"}>
-            <div className={"flex-col justify-center items-center text-gray-500"}>
-                <div className={"flex"}>
-                    <a className={"mx-2"}>서비스 소개</a> |
-                    <a className={"mx-2"}>이용약관</a> |
-                    <a className={"mx-2"}>디렉토리</a> |
-                    <a className={"mx-2"}>개인정보 처리방침</a> |
-                    <a className={"mx-2"}>신고 가이드</a>
-                </div>
-                <div className={"flex mt-3 text-gray-400"}>
-                    @2022 Prismweaver Inc.
-                </div>
-            </div>
-            <div className={"flex text-base"}>
-                <button className={"mx-2 px-7 py-3 bg-zinc-200 rounded-full"}>
+        <div className={"lg:hidden grid grid-cols-2 gap-2 text-lg mt-2 border-t-2 mx-2 text-gray-500 p-2"}>
+            <a>서비스 소개</a>
+            <a>이용약관</a>
+            <a>디렉토리</a>
+            <a>개인정보 처리방침</a>
+            <a>신고 가이드</a>
+            <a href=""></a>
+            <span className={"col-span-2"}>@2022 Prismweaver Inc.</span>
+            <div className={"col-span-2 text-base flex justify-center text-black"}>
+                <button className={"px-7 py-3 bg-zinc-200 rounded-full"}>
                     <FontAwesomeIcon icon={faApple} className={"mr-3"}/>
                     APP STORE
                 </button>
@@ -62,5 +80,35 @@ export default function Layout({
                 </button>
             </div>
         </div>
+
+        <div className={"lg:block hidden"}>
+            <div className={"grid grid-cols-2 gap-2 border-t-2 mx-2 text-gray-500 p-2"}>
+                <div className={"flex space-x-5"}>
+                    <a>서비스 소개</a>
+                    <span>|</span>
+                    <a>이용약관</a>
+                    <span>|</span>
+                    <a>디렉토리</a>
+                    <span>|</span>
+                    <a>개인정보 처리방침</a>
+                    <span>|</span>
+                    <a>신고 가이드</a>
+                </div>
+                <div className={"row-span-2 flex justify-end items-center text-black"}>
+                    <button className={"px-7 py-3 bg-zinc-200 rounded-full"}>
+                        <FontAwesomeIcon icon={faApple} className={"mr-3"}/>
+                        APP STORE
+                    </button>
+                    <button className={"mx-2 px-7 py-2 bg-zinc-200 rounded-full"}>
+                        <FontAwesomeIcon icon={faGooglePlay} className={"mr-3"}/>
+                        GOOGLE PLAY
+                    </button>
+                </div>
+                <div>
+                    <span>@2022 Prismweaver Inc.</span>
+                </div>
+            </div>
+        </div>
+
     </div>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,7 @@ import Layout from "@components/layout";
 const Home: NextPage = () => {
     return (
         <Layout title="kimsajang">
-
             <h1 className="text-blue-500"> Hello </h1>
-
         </Layout>
     )
 }
